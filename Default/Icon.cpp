59703600e5407546cf9c978c1151627df47a9650
panggee12 <rkdtmdwls52@suwon.ac.kr>
2022-06-12 19:29:50 +0900

    아이콘 오브젝트 추가

---
+++ Default/Icon.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "Icon.h"
+
+
+CIcon::CIcon()
+{
+}
+
+
+CIcon::~CIcon()
+{
+}
+
+void CIcon::Initialize(void)
+{
+}
+
+int CIcon::Update(void)
+{
+	return 0;
+}
+
+void CIcon::Late_Update(void)
+{
+}
+
+void CIcon::Render(HDC hDC)
+{
+}
+
+void CIcon::Release(void)
+{
+}
